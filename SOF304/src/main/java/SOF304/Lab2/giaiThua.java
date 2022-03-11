18343d369e7ad4a2e2f93b3fb98eb55ad7d8da4f
hieubqph13812 <bui.quang.hieu2910@gmail.com>
2022-03-11 22:35:19 +0700

    hieubq

--- SOF304/src/main/java/SOF304/Lab2/giaiThua.java
+++ SOF304/src/main/java/SOF304/Lab2/giaiThua.java
@@ -7,6 +7,10 @@ public class giaiThua {
 		if (input == 0 || input == 1) {
 			return 1;
 		}
+		if (input <0 || input >21) {
+			System.out.println("Giá trị đầu vào không đúng");
+			return -1;
+		}
 		long plus = 1;
 		for (int i = 1; i <= input; i++) {
 			plus *= i;
