34310d543b0da76f6f3f888b59bbd3b892d0043a
hieubqph13812 <bui.quang.hieu2910@gmail.com>
2022-03-15 17:06:24 +0700

    hieubq

---
+++ SOF304/src/test/java/SOF304/Lab1/Runner.java
@@ -0,0 +1,18 @@
+package SOF304.Lab1;
+
+import org.junit.runner.JUnitCore;
+
+/**
+ * @author BUI_QUANG_HIEU
+ *
+ */
+public class Runner {
+	public static void main(String[] args) {
+		JUnitCore runner = new JUnitCore();
+		org.junit.runner.Result result = runner.run(TestSuite.class);
+		System.out.println("run tests: "+ result.getRunCount());
+		System.out.println("failed tests: "+ result.getFailureCount());
+		System.out.println("ignored tests: "+ result.getIgnoreCount());
+		System.out.println("success: "+ result.wasSuccessful());
+	}
+}
--- SOF304/src/test/java/SOF304/Lab1/TestSuite.java
+++ SOF304/src/test/java/SOF304/Lab1/TestSuite.java
@@ -6,7 +6,7 @@ import org.junit.runners.Suite;
 @RunWith(Suite.class)
 @Suite.SuiteClasses(
 
-{Lab1_3Test.class}
+{Lab1_3Test.class, CalulatorTest.class}
 )
 public class TestSuite{ // nothing
 }
