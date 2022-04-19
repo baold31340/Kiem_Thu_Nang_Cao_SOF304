647a862f156324ed9cb9cdc67b9267a80d627dda
hieubqph13812 <bui.quang.hieu2910@gmail.com>
2022-04-19 22:21:54 +0700

    hieubq

---
+++ KiemTraThu/src/test/java/pass2/TC1.java
@@ -0,0 +1,101 @@
+package pass2;
+
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.testng.annotations.BeforeTest;
+
+import static org.testng.Assert.assertEquals;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.AfterTest;
+
+public class TC1 {
+
+	WebDriver driver;
+
+	@Test
+	public void f() {
+
+		// bấm link account
+		driver.findElement(By.xpath("//header/div[1]/div[2]/div[1]/a[1]/span[2]")).click();
+
+		// bấm my account
+		driver.findElement(By.xpath("//header/div[1]/div[5]/div[1]/ul[1]/li[1]/a[1]")).click();
+
+		// create
+		driver.findElement(By.xpath("//span[contains(text(),'Create an Account')]")).click();
+
+		// nhập thông tin
+		driver.findElement(By.xpath("//input[@id='firstname']")).sendKeys("Bui");
+		driver.findElement(By.xpath("//input[@id='middlename']")).sendKeys("Quang");
+		driver.findElement(By.xpath("//input[@id='lastname']")).sendKeys("Hieu");
+		driver.findElement(By.xpath("//input[@id='email_address']")).sendKeys("hieubq123458@gmail.com");
+		driver.findElement(By.xpath("//input[@id='password']")).sendKeys("123456");
+		driver.findElement(By.xpath("//input[@id='confirmation']")).sendKeys("123456");
+		driver.findElement(By.xpath("//label[contains(text(),'Sign Up for Newsletter')]")).click();
+		driver.findElement(By.xpath("//span[contains(text(),'Register')]")).click();
+
+		// Lấy dòng chữ hello
+		String hello = driver.findElement(By.xpath("//strong[contains(text(),'Hello, Bui Quang Hieu!')]")).getText();
+
+		// Kiểm tra
+
+		assertEquals(hello, "Hello, Bui Quang Hieu!");
+
+		// TV
+		driver.findElement(By.xpath("//a[contains(text(),'TV')]")).click();
+
+		// Thêm vào yêu thích
+		driver.findElement(By.xpath(
+				"//body/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[2]/ul[1]/li[1]/div[1]/div[3]/ul[1]/li[1]/a[1]"))
+				.click();
+
+		// Bấm share washlist
+		driver.findElement(By.xpath("//span[contains(text(),'Share Wishlist')]")).click();
+
+		// chèn email
+		driver.findElement(By.xpath("//textarea[@id='email_address']")).sendKeys("abc@gmail.com");
+
+		// Bấm share
+		driver.findElement(By.xpath("//span[contains(text(),'Share Wishlist')]")).click();
+
+		// kiểm tra
+		String sendString = driver.findElement(By.xpath("//span[contains(text(),'Your Wishlist has been shared.')]"))
+				.getText();
+		assertEquals(sendString, "Your Wishlist has been shared.");
+
+	}
+
+	@Test
+	public void p1() {
+		
+		//Test case liên quan đến select
+		// TV
+		driver.findElement(By.xpath("//a[contains(text(),'TV')]")).click();
+		
+		//Dùng hàm select
+		Select select = new Select(driver.findElement(
+				By.xpath("//body/div[1]/div[1]/div[2]/div[1]/div[2]/div[1]/div[2]/div[1]/div[1]/div[1]/select[1]")));
+		
+		// Selected tại vị trí index
+		select.selectByIndex(0);
+	}
+
+	@BeforeTest
+	public void beforeTest() {
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.get("http://live.techpanda.org/");
+	}
+
+	@AfterTest
+	public void afterTest() {
+//		driver.close();
+	}
+
+}
